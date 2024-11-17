79472c5ce090125c002093f0889cafe3291dc7dc
bubbaboogs <bubbaboogs5@gmail.com>
2024-11-16 23:03:33 -0500

    stat modifcation havent tested

---
+++ Modification/PlayerStats.cs
@@ -0,0 +1,31 @@
+using GFApi;
+using UnityEngine;
+
+namespace GFApi.Modification{
+    public static class PlayerStats{
+        public enum stat{
+            glim,
+            starRank,
+            clickGrowStrength,
+            xp
+        }
+        public static void ModifyStats(stat statToModify, int newStat){
+            PlayerData playerData = new PlayerData();
+            PlayerData.PlayedBiomeData playedBiomeData = new PlayerData.PlayedBiomeData();
+            switch(statToModify){
+                case stat.clickGrowStrength:
+                    playerData.clickGrowStrength = newStat;
+                    break;
+                case stat.glim:
+                    playedBiomeData.glimAmount = newStat;
+                    break;
+                case stat.starRank:
+                    playerData.starRank = newStat;
+                    break;
+                case stat.xp:
+                    playedBiomeData.xp = newStat;
+                    break;
+            }
+        }
+    }
+}
